2de7a2277034fe4964ef9a10eb371b9897a48117
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-01 17:05:24 +0800

    Add rest framework settings

--- core/settings.py
+++ core/settings.py
@@ -115,10 +115,20 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
